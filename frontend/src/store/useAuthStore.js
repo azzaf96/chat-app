52dbb9c8b357173334e2f02b129f129e8bff8786
azzafk96 <azzafeki96@gmail.com>
2025-11-18 08:34:36 +0100

    feat: update frontend dependencies and implement routing
    - Added axios, lucide-react, react-hot-toast, react-router-dom, zustand, autoprefixer, daisyui, postcss, and tailwindcss to dependencies.
    - Removed App.css and replaced it with Tailwind CSS setup in index.css.
    - Refactored App.jsx to implement routing and authentication checks.
    - Created Navbar component for navigation and user actions.
    - Implemented pages for Home, Login, SignUp, Profile, and Settings.
    - Added AuthImagePattern component for visual enhancement on SignUp page.
    - Established useAuthStore for managing authentication state and actions.
    - Configured PostCSS with Tailwind CSS and DaisyUI.

---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,49 @@
+import {create} from 'zustand'
+import { axiosInstance } from '../lib/axios.js';
+import toast from 'react-hot-toast';
+
+export const useAuthStore = create((set) => ({
+  authUser: null,
+  isSigningUp: false,
+  isLoggingIn: false,
+  isUpdatingProfile: false,
+
+  isCheckingAuthStatus: true,
+
+  checkAuth: async () => {
+    try {
+        const res = await axiosInstance.get("/auth/check");
+        set({authUser: res.data})
+    } catch (error) {
+        console.log("Error in checkAuth:", error);
+        
+      set({authUser: null})
+    } finally {
+        set({isCheckingAuthStatus: false})
+    }
+  },
+
+  signup: async (data) => {
+    set({isSigningUp: true});
+    try {
+      const res = await axiosInstance.post("/auth/signup", data);
+      set({authUser: res.data});
+      toast.success("Signup successful!");
+    } catch (error) {
+      toast.error(error.response?.data?.message || "Signup failed");
+    } finally {
+      set({isSigningUp: false});
+    }
+  },
+
+  logout: async () => {
+    try {
+      await axiosInstance.post("/auth/logout");
+      set({authUser: null});
+      toast.success("Logged out successfully");
+    } catch (error) {
+      toast.error(error.response?.data?.message || "Logout failed");
+    }
+  },
+
+}));
